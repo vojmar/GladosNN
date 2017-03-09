714e04e80bfb4bfbfcefd86e0911a9e048c37150
vojmar <marsa33bsu@seznam.cz>
2017-03-09 16:11:38 +0100

    Meh

---
+++ Glados/GladosBrain/Neuron.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladosBrain
+{
+    class Neuron
+    {
+        Dictionary<Neuron, uint> synapses;
+        public Neuron(Neuron[] inputN)
+        {
+            synapses = new Dictionary<Neuron, uint>();
+            foreach (var neuron in inputN)
+            {
+                synapses.Add(neuron,uint.MaxValue/2);
+            }
+        }
+    }
+}
